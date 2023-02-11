24e8d57d2f57fc8c26ae7af161af202ac96fd8d6
hase75 <122374696+hase75@users.noreply.github.com>
2023-02-11 07:17:53 +0000

    [Finish]4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,14 +14,14 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
